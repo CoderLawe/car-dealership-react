4e2b066c984638912f0972ed1954b00b975ac370
Lawe Sosah <lawesosah@Lawes-MacBook-Pro.local>
2023-05-10 10:11:14 +0200

    Finalized and optimized car cards

--- src/components/CarCardMain.jsx
+++ src/components/CarCardMain.jsx
@@ -1,13 +1,69 @@
 import React from 'react'
-
-function CarCardMain() {
+import { AiOutlineClockCircle } from "react-icons/ai"
+import { VscGear } from "react-icons/vsc";
+import { BsFillFuelPumpFill } from "react-icons/bs";
+function CarCardMain({type, title, image, price, fuel, mileage, transmission }) {
   return (
-    <div className="block relative">
-        <img className="h-[218.08px] w-[332.66px]" src="https://media.carsandbids.com/cdn-cgi/image/width=712,height=468,quality=70/20309e251cb7341d1fb94cb5d4546882260d2202/photos/9a4RoxD8-W1q8Ccxvvz-(edit).jpg?t=168271134231"/>
+    <div className="block">
+        <div className="relative">
+            
+            <img className="h-[218.08px] w-[332.66px] object-cover" src={image}/>
 
-        <div className="bg-[#262626]/80 absolute bottom-[4px] left-[8px] h-[25px] w-[179.86px] flex justify-between rounded-[6px]">
+
+            <div className="bg-[#262626] absolute bottom-[4px] left-[8px] h-[25px] w-[179.86px] flex justify-between items-center rounded-[6px]">
+                {/* Left side */}
+                <div className="flex space-x-[4px] items-center ml-[8px]">
+                    <AiOutlineClockCircle className="h-[11px] w-[11px] text-gray-400" />
+                    <p className="text-[13px] text-white font-[500]">{type}</p>
+
+                </div>
+
+
+                {/* Right side */}
+
+                <div className="mr-[7.85px]">
+                    <p className="text-[14px] text-white font-[500]">BIF {price}</p>
+                </div>
+
+            </div>
+
+            
+        </div>
+
+
+        <div className="block">
+            {/* Bottom details */}
+            
+            <p className="text-[16px] font-[700] text-[#262626] mt-[15px]">{title}</p>
+
+            <div className="flex space-x-[6.89px]">
+                <div className="bg-[#5CA1FF] rounded-[4px] h-[19px] w-[73.39px] text-center pt-[2px] text-white text-[10px]">New</div>
+                <p className="text-[#262626] text-[14px] font-[400]">{mileage} Kms</p>
+
+            </div>
+
+            <div className="flex items-center mt-[10px] space-x-[12px]">
+
+                <div className="mt-[4px] flex items-center space-x-[8px]">
+                    <VscGear className="text-gray-500 text-[20px]"/>
+                    <p className="text-[#262626] text-[14px] font-[600]">{transmission}</p>
+                </div>
+
+                <div className="flex items-center space-x-[8px]">
+                    <BsFillFuelPumpFill className="text-red-600 text-[20px]"/>
+                    <p className="text-[#262626] text-[14px] font-[600]">{fuel}</p>
+
+
+                </div>
+
+            </div>
+
+
+            <button className=" h-[40px] w-full text-[#F75D34] bg-white border border-[#F75D34] text-[12px] mt-[16px] cursor-pointer hover:bg-[#F75D34] hover:text-gray-100 transform transition-all duration-300 ease-out">More Details</button>
+
         </div>
     </div>
+   
   )
 }
 
--- src/components/CarCardsMain.jsx
+++ src/components/CarCardsMain.jsx
@@ -10,8 +10,17 @@ function CarCardsMain() {
             <p className="text-gray-900 text-[24px] font-[700]">Arrivals</p>
             <Filters />
         </div>
-        <div className="flex space-x-[8px] mt-[30px]">
-            <CarCardMain />
+        <div className="grid lg:grid-cols-3 gap-x-[28px] gap-y-[44.01px] mt-[30px]">
+            <CarCardMain type="sedan" price="20,000,000" title="2003 BMW 745i" transmission="Manual" fuel="Petrol" image="https://media.carsandbids.com/cdn-cgi/image/width=712,height=468,quality=70/20309e251cb7341d1fb94cb5d4546882260d2202/photos/9a4RoxD8-W1q8Ccxvvz-(edit).jpg?t=168271134231" mileage="25,000"/>
+            <CarCardMain type="SUV" price="15,000,000" title="Jeep Liberty 2005 Renegade" transmission="Automatic" fuel="Petrol" mileage="161,800" image="https://cdn.discordapp.com/attachments/817048198022430761/1105767736648421426/IMG_20230429_064010_723.jpg"/>
+            <CarCardMain type="SUV" price="15,000,000" title="2002 Toyota 4Runner SR5 4x4" transmission="Manual" fuel="Petrol" mileage="161,800" image="https://media.carsandbids.com/cdn-cgi/image/width=712,height=468,quality=70/438ad923cef6d8239e95d61e7d6849486bae11d9/photos/36Y0lpRn-37PL0UNo4H-(edit).jpg?t=168236584346"/>
+
+
+            <CarCardMain type="SUV" price="15,000,000" title="Jeep Liberty 2005 Renegade" transmission="Automatic" fuel="Petrol" mileage="161,800" image="https://cdn.discordapp.com/attachments/817048198022430761/1105767736648421426/IMG_20230429_064010_723.jpg"/>
+            <CarCardMain type="sedan" price="20,000,000" title="2003 BMW 745i" transmission="Manual" fuel="Petrol" image="https://media.carsandbids.com/cdn-cgi/image/width=712,height=468,quality=70/20309e251cb7341d1fb94cb5d4546882260d2202/photos/9a4RoxD8-W1q8Ccxvvz-(edit).jpg?t=168271134231" mileage="25,000"/>
+            <CarCardMain type="SUV" price="15,000,000" title="2002 Toyota 4Runner SR5 4x4" transmission="Manual" fuel="Petrol" mileage="161,800" image="https://media.carsandbids.com/cdn-cgi/image/width=712,height=468,quality=70/438ad923cef6d8239e95d61e7d6849486bae11d9/photos/36Y0lpRn-37PL0UNo4H-(edit).jpg?t=168236584346"/>
+
+
         </div>
     </div>
   )
